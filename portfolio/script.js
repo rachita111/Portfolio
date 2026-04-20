f1fecabd0c900f09fffc7ff8ea3a5f7a615da7ac
rachita111 <rachitatokas@gmail.com>
2026-04-20 20:00:00 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+function scrollToContact() {
+  document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToProjects() {
+  document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+}
