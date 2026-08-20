9ac6288c63842dc51f5f3e1c2e2e11c714944a4a
rachita111 <rachitatokas@gmail.com>
2026-08-20 12:13:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (29.73%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        for(int i=0;i<numRows;i++){
+            vector<int> ans;
+            for(int j=0;j<i+1;j++){
+                if(j==0 || j==i) ans.push_back(1);
+                else ans.push_back(res[i-1][j-1]+res[i-1][j]);
+            }
+            res.push_back(ans);
+        }
+        return res;
+    }
+};
