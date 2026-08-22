da4eb190065b3879ae21486b01a5876e1bbf3d6c
rachita111 <rachitatokas@gmail.com>
2026-08-22 18:36:55 +0530

    Time: 3 ms (89.13%) | Memory: 71.6 MB (46.43%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,51 +1,37 @@
 class Solution {
 public:
-    int goalCnt(vector<int>& nums,int goal){
-        if(goal<0) return 0;
-        int right=0,left=0,cnt=0;
-        long long sum=0;
-        while(right<nums.size()){
-            sum+=nums[right];
-            while(sum>goal){
-                sum-=nums[left];
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int right = 0, left = 0;
+        int cnt = 0, ans = 0;
+
+        while (right < nums.size()) {
+
+            if (nums[right] % 2 == 1) {
+                cnt++;
+            }
+
+            while (cnt > k) {
+                if (nums[left] % 2 == 1) {
+                    cnt--;
+                }
                 left++;
             }
-            cnt+=(right-left+1);
+
+            if (cnt == k) {
+                int temp = left;
+
+                // Count how many even elements
+                // can be removed from the left
+                while (temp < nums.size() && nums[temp] % 2 == 0) {
+                    temp++;
+                }
+
+                ans += temp - left + 1;
+            }
+
             right++;
         }
-        return cnt;
-    }
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return (goalCnt(nums,goal)-goalCnt(nums,goal-1));
-    }
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2==1) nums[i]=1;
-            else if(nums[i]%2==0) nums[i]=0;
-        }
-        return numSubarraysWithSum(nums,k);
+
+        return ans;
     }
 };
-
-
-// class Solution {
-// public:
-//     int numberOfSubarrays(vector<int>& nums, int k) {
-//         int right=0,left=0,cnt=0,ans=0,evenC=0;
-//         while(right<nums.size()){
-//             if(nums[right]%2==1){
-//                 cnt++;
-//                 right++;
-//             }else if(nums[right]%2==0){
-//                 if(cnt==0) evenC++;
-//                 right++;
-//             }
-//             if(cnt==k) ans++;
-//             else if(cnt>k){
-//                 left++;
-//                 if(nums[left]%2==1) cnt--;
-//             }
-//         }
-//         return ans;
-//     }
-// };
